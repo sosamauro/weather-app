e2f040e5656931011286424efed44e66c914e11f
Mauro Sosa <mauro4sosa@gmail.com>
2022-03-16 14:27:18 -0300

    geolocation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,6 +122,66 @@ let weather = {
   },
 };
 
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var api_key = "d6e5abf628d442ccaafa9da883ac74d4";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      api_key +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status === 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.town);
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+  getLocation: function () {
+    function success(data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    } else {
+      weather.fetchWeather("Buenos Aires");
+    }
+  },
+};
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
@@ -132,4 +192,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (e) {
   }
 });
 
-weather.fetchWeather("Buenos Aires");
+geocode.getLocation();
